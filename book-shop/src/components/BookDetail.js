184490112b245cd0385a70ad07ab83663c2877bb
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2021-10-08 16:23:07 +0330

    delete book is handle

--- book-shop/src/components/BookDetail.js
+++ book-shop/src/components/BookDetail.js
@@ -1,15 +1,18 @@
 
-import React from 'react'
+import React, { useContext } from 'react'
+import { BookListProvider } from '../context/BooksListContext';
 
-function BookDetail({bookInfo}) {
+function BookDetail({ bookInfo }) {
+    const { removeBook } = useContext(BookListProvider);
 
+    const deleteBook = () => {
+        removeBook(bookInfo.id);
+    }
     return (
         <div>
-
-            <li>
-                <div>{bookInfo.title}</div>
-                <div>{bookInfo.author}</div>
-
+            <li onClick={deleteBook}>
+                <div className="title">{bookInfo.title}</div>
+                <div className="author">{bookInfo.author}</div>
             </li>
         </div>
     )
--- book-shop/src/components/BooksList.js
+++ book-shop/src/components/BooksList.js
@@ -1,18 +1,25 @@
-import React , {useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
 import { BookListProvider } from '../context/BooksListContext';
 import BookDetail from './BookDetail';
 
 function BooksList() {
-    const {books} = useContext(BookListProvider);
+    const { books } = useContext(BookListProvider);
 
 
 
     return (
         <div className="book-list">
-            <ul>
-                {books.map((book)=> <BookDetail  bookInfo={book} key={book.id}/>)}
-            </ul>
-           
+            {books.length > 0 ?
+                <ul>
+                    {books.map((book) => <BookDetail bookInfo={book} key={book.id} />)}
+                </ul>
+
+                :
+                <h3>
+                    There is no book in the List
+                </h3>
+                }
+
         </div>
     )
 }
